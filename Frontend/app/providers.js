3cfe6e8795b0be356e090902cd25f44caccb5855
aditya-raulji <hemangraulji51@gmail.com>
2025-10-11 07:16:03 +0530

    feat: add context providers

---
+++ Frontend/app/providers.js
@@ -0,0 +1,29 @@
+'use client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ChakraProvider } from '@chakra-ui/react'
+import { MantineProvider } from '@mantine/core'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import ErrorBoundary from '@/components/ErrorBoundary'
+import { AnimatePresence } from 'framer-motion'
+import { useState } from 'react'
+
+export default function Providers({ children }) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <MantineProvider>
+            <AnimatePresence mode="wait" initial={false}>
+              {children}
+            </AnimatePresence>
+            <ToastContainer position="top-right" theme="colored" />
+            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+          </MantineProvider>
+        </ChakraProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
+  )
+}
